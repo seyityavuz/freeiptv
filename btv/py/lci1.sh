3518df18d657bc5470aa5cee126f34c69c88b3eb
seyityavuz <seyityavuz@gmail.com>
2025-10-17 17:12:31 +0300

    Delete btv/py directory

--- btv/py/lci1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/btv/py/lci1.py > ressources/btv/py/lci1.m3u8
-
-exit 0
--- btv/py/lcia.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/btv/py/lci.py > ressources/btv/py/lci.m3u8
-
-exit 0
